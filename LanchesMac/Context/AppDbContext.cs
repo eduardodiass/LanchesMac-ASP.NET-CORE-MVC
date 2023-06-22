b465af9a07c4c35dcd5ed0b8feea9c74e53754f0
Eduardo Rodrigues <eduardo.david.dias@gmail.com>
2023-06-22 13:11:47 -0300

    Configurando Session e HttpContext

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace LanchesMac.Context
 {
     public class AppDbContext : DbContext
-    {
+    { 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
