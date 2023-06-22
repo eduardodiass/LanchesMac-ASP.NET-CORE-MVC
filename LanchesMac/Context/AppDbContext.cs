7b2dc0f377262562e341f870ad798d35e10f0e7c
Eduardo Rodrigues <eduardo.david.dias@gmail.com>
2023-06-22 12:38:53 -0300

    Acréssimo do CarrinhoCompraItem no modelo de dominio e criação da tabela no SQL atraves do Entity Framework

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -12,6 +12,8 @@ namespace LanchesMac.Context
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }  
+
 
 
     }
