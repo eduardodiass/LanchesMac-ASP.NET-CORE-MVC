2d574fc5c78854797095856f6d328d5e5ac9c639
Eduardo Rodrigues <eduardo.david.dias@gmail.com>
2023-08-11 23:58:09 -0300

    criação da funcionalidade de gerenciamento de imagens na area administrativa

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -32,7 +32,8 @@ public class Startup
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
         services.AddScoped<RelatorioVendasService>();
-        
+
+        services.Configure<ConfigurationImagens>(Configuration.GetSection("ConfigurationPastaImagens"));
         services.AddAuthorization(options =>{options.AddPolicy("Admin",politica =>{ politica.RequireRole("Admin"); }); });
 
 
